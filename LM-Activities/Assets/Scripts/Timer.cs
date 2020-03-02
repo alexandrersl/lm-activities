48e61d209548cde3b089025a94b672a05dac8f4a
Faycalth <faycalthamri38@gmail.com>
2020-03-02 22:19:19 +0100

    ajout du spawner/destroyer et amelioration du timer

---
+++ LM-Activities/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+	public float timeStart = 5; 
+    public Text textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= 1 * Time.deltaTime;
+        textBox.text = Mathf.Round(timeStart).ToString(); 
+
+        if (timeStart <= 0){
+        	textBox.color = Color.red;
+        	timeStart =0;
+        	textBox.text = timeStart.ToString();
+        }
+    }
+}
