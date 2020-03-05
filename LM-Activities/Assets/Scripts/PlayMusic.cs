fe313c75423c52b23e68d586ab2316c0be6a01ac
Alexandre Roussel <alexandreroussel.fr@gmail.com>
2020-03-05 08:54:30 -0500

    scene Music

---
+++ LM-Activities/Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public string name;
+    public void PlaySound()
+    {
+        FindObjectOfType<AudioManager>().Play(name);
+    }
+}
