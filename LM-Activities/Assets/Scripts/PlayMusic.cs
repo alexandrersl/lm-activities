140669b07d507b9b4c077e9043bf02afe1fcb297
Alexandre Roussel <2001024@cgmatane.qc.ca>
2020-03-12 10:35:07 -0400

    modif de la scene LancerFranc

--- LM-Activities/Assets/Scripts/PlayMusic.cs
+++ LM-Activities/Assets/Scripts/PlayMusic.cs
@@ -7,6 +7,7 @@ public class PlayMusic : MonoBehaviour
     public new string name;
     public void PlaySound()
     {
+        Debug.Log("roblox");
         FindObjectOfType<AudioManager>().Play(name);
     }
 }
